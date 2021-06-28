da2e45234ff9b527439314c70dfb9ca003e0e508
Lorenzo Venturelli <venturelli.lorenzo@outlook.it>
2021-06-28 15:00:58 +0200

    Telegram Bot with Asyncio
    Started to work on implementing the Telegram Bot with Asyncio.
    It will be necessary to expand the MySQL module in order to manage everything better

--- Server/Python/telegramBot.py
+++ Server/Python/telegramBot.py
@@ -1,4 +1,4 @@
-import time, datetime, threading, logging
+import time, datetime, threading, logging, asyncio
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from interfaces import Data, Event, System
 from db import MySQL
@@ -34,6 +34,9 @@ class TelegramBot(threading.Thread):
         self._commandHandlers = list()
         self._periodicUpdateThread = None
 
+        self._asyncLoop = None
+        self._periodicUpdateTask = None
+
         self._isRunning = True
         super().__init__(daemon = False, name = "Telegram Bot")
 
@@ -52,16 +55,40 @@ class TelegramBot(threading.Thread):
     def _showBrief(self, update: dict, context: object) -> None:
         pass
 
-    def _sendPeriodicUpdate(self) -> None:
-        pass
-    
+    async def _sendPeriodicUpdate(self) -> None:
+        '''
+        Task that sends updates to every subscribed user
+        '''
+
+        while self._isRunning == True:                                          # Of course if the thread is dead, this routine must return as well but we expect to cancel the task anyway
+            
+            # This task must wake up every "botUpdatePeriod" hours to send all the updates
+            await asyncio.sleep(delay = self._system.settings["botUpdatePeriod"] * 3600)
+
+            # UserList dovrebbe essere un dizionario con tutte le info dell'utente. Prima di proseguire con le select è meglio verificare che 
+            # quell'utente voglia l'aggiornamento proprio adesso (comunque multiplo di 4)
+            # Dal dizionario si tira fuori la chiave che è il chat id e si prendono i sensori, dai quali si prende il record più recente
+            # Si manda un messaggio all'utente dicendo "Ciao UTENTE, ecco i tuoi aggiornamenti in tempo reale..."
+
+            userList = await self._db.getUserList()                                     # Get a list of subscribed users
+            for user in userList:                                                       # For each user
+                cityList = await self._db.getSensorsByUser(userID = user)               # Get a list of associated cities
+                updatePacket = await self._db.getUpdateByCity(cities = cityList)        # For all the associated cities, get the most recent record
     def stopThread(self) -> None:
         '''
         Close this Telegram Bot Thread
         '''
         if self._isRunning == True:
             try:
-                self._periodicUpdateThread.cancel()                         # Stop the periodic update routine
+                if self._asyncLoop != None:
+                    if self._periodicUpdateTask != None:
+                        self._periodicUpdateTask.cancel()                       # Cancel the periodic task
+                    self._asyncLoop.stop()                                      # Stop the event loop
+            except Exception as e:
+                self._logger.critical("Impossible to gracefully terminate the Telegram Bot event loop!", exc_info = True)
+                raise e
+
+            try:
                 self._updater.stop()                                        # Shut the Telegram Bot down
                 self._db.close()                                            # Close the connection to DB
                 self._isRunning = False
@@ -97,18 +124,31 @@ class TelegramBot(threading.Thread):
             self._logger.critical("Impossible to register command handlers to the Telegram Bot's dispatcher", exc_info = True)
             self._isRunning = False
 
-        # The bot is ready to run, let's start listenig and let's create the periodic routine to send updates
+        # Create the Async event loop for this thread and create the main bot task as well as the periodic update task
+        try:
+            self._asyncLoop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self._asyncLoop)
+            self._periodicUpdateTask = self._asyncLoop.create_task(self._sendPeriodicUpdate)
+        except Exception:
+            self._logger.critical("Impossible to set up Async environment for Telegram Bot Thread", exc_info = True)
+            self._isRunning = False
+
+        # The bot is ready to run, let's start listenig and let's start the event loop
         if self._isRunning == True:
+            try:
+                self._updater.start_polling()                           # Enable the Telegram Server polling, looking for new messages
+                self._logger.debug("Telegram Bot's polling started")
+            except Exception:
+                self._logger.critical("An error occurred while starting the bot polling", exc_info = True)
+                self._isRunning = False
 
-            # Create a periodic timer that executes every "botUpdatePeriod" hours and send to every subscribed user an update
-            self._periodicUpdateThread = threading.Timer(interval = (self._system.settings["botUpdatePeriod"] * 3600), function = self._sendPeriodicUpdate)
-            self._periodicUpdateThread.start()
+        if self._isRunning == True:
+            try:  
+                self._asyncLoop.run_forever()                           # Start the Async loop and hang here
+            except asyncio.CancelledError:
+                self._logger.debug("Main loop raised a CancellatedError")
+            except Exception:
+                self._logger.critical("An error occurred while starting the bot polling or the Bot Async Loop", exc_info = True)
 
-            self._updater.start_polling()                           # Enable the Telegram Server polling, looking for new messages
-            self._logger.debug("Telegram Bot's polling started")
-            self._updater.idle()                                    # Put this thread in sleeping mode
-
-        
-        
         self._logger.debug("Telegram Bot's thread terminated")
         return
